caca94965bd23d5f3d087c1047ef17af5e72e8a0
PeterisP <peterisp@gmail.com>
2018-04-06 22:31:03 +0300

    Fix for stem data format

--- tezaurs_dump.py
+++ tezaurs_dump.py
@@ -34,9 +34,10 @@ def fetch_lexemes():
     global connection
     cursor = connection.cursor(cursor_factory=NamedTupleCursor) 
     sql = """
-select l.id as lexeme_id, e.id as entry_id, e.human_id, paradigm_id, l.data, stem1, stem2, stem3, lemma
+select l.id as lexeme_id, e.id as entry_id, e.human_id, p.legacy_id as paradigm_id, l.data, stem1, stem2, stem3, lemma
 from tezaurs.lexemes l
 join tezaurs.entries e on l.entry_id = e.id
+join tezaurs.paradigms p on l.paradigm_id = p.id
 where l.type_id = 1 -- words, not named entities or MWE's
 """
 # TODO - filtrs uz e.release_id lai ņemtu svaigāko relīzi nevis visas
@@ -58,17 +59,32 @@ where l.type_id = 1 -- words, not named entities or MWE's
                 'lemma'     : row.lemma
             }
             if row.stem1:
-                lexeme['stem1'] = row.stem1
+                lexeme['stem1'] = row.stem1[1:-1] # noņemam {}
             if row.stem2:
-                lexeme['stem2'] = row.stem2
+                if ',' in row.stem2:
+                    print('Dubultcelms %s'%(row.stem2, ))
+                    continue
+                lexeme['stem2'] = row.stem2[1:-1] # noņemam {}
             if row.stem3:
-                lexeme['stem3'] = row.stem3
+                if ',' in row.stem3:
+                    print('Dubultcelms %s'%(row.stem3, ))
+                    continue
+                lexeme['stem3'] = row.stem3[1:-1] # noņemam {}
             if row.data:
                 dati = row.data
                 gram = dati.get('Gram')
                 if gram and gram.get('Flags'):
+                    flags = dict(gram.get('Flags'))
+                    for key in gram.get('Flags'):   
+                        value = flags[key]
+                        if type(value) is list and len(value) == 1:
+                            flags[key] = value[0]
+                            value = flags[key]
+                        if key == 'Dzimte' and type(value) is not list and value.endswith(' dzimte'):
+                            flags[key] = value[:-7]
+                            value = flags[key]
                     gram = dict(gram)
-                    gram.update(gram.get('Flags'))
+                    gram.update(flags)
                     del gram['Flags']
 
                 if dati.get('Pronunciation'):
